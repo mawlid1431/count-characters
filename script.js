898ebacfd3393b8d999473e1476ec19851d7a2a4
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 15:25:40 +0300

    script.js I added New code :x

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Good Luck 💪🏾
+// Good Luck 💪🏾  
+
+
+
+function count(char, str) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// Example usages:
+console.log(count("a", "fikrcamp")); // Output: 1
+console.log(count("c", "bootcamp")); // Output: 1
+console.log(count("o", "fikrcamp coding bootcamp")); // Output: 3
+
